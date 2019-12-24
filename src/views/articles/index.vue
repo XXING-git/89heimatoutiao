138eb70689d549f671550d3ccafe88e3b864cc10
dhx <you@example.com>
2019-12-24 20:55:25 +0800

    黑马头条PC-内容列表-请求数据

--- src/views/articles/index.vue
+++ src/views/articles/index.vue
@@ -9,12 +9,12 @@
         <span>文章状态</span>
       </el-col>
       <el-col :span="18">
-          <!--
+        <!--
               0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除，不传为全部
-           -->
+        -->
         <!-- 单选框组 -->
         <el-radio-group v-model="formData.status">
-            <!-- 全部这个5是默认的,在传参的时候判断一下 是不是5 如果是5 就传个null -->
+          <!-- 全部这个5是默认的,在传参的时候判断一下 是不是5 如果是5 就传个null -->
           <el-radio :label="5">全部</el-radio>
           <el-radio :label="0">草稿</el-radio>
           <el-radio :label="1">待审核</el-radio>
@@ -29,13 +29,11 @@
       </el-col>
       <el-col :span="18">
         <el-select v-model="formData.channel_id">
-            <!-- 循环生成多个el-option
+          <!-- 循环生成多个el-option
               label 指的是 el-option显示值
               value指的是 el-option的存储值
-            -->
-            <el-option v-for="item in channels" :key="item.id"
-             :label="item.name"
-             :value="item.id"></el-option>
+          -->
+          <el-option v-for="item in channels" :key="item.id" :label="item.name" :value="item.id"></el-option>
         </el-select>
       </el-col>
     </el-row>
@@ -53,30 +51,41 @@
         ></el-date-picker>
       </el-col>
     </el-row>
-        <!-- 主体 -->
-    <el-row class='total'>
-        <span>共找到1000条符合条件的内容</span>
+    <!-- 主体 -->
+    <el-row class="total">
+      <span>共找到1000条符合条件的内容</span>
     </el-row>
     <!-- 循环的模板 -->
-    <el-row  v-for="item in 100" :key="item" class='article-item' type='flex' justify="space-between">
-        <!-- 左侧 -->
-       <el-col :span="14">
-           <el-row type='flex'>
-             <img src="../../assets/img/q39.jpg" alt="">
-              <div class='info'>
-                <span>年少不听李宗盛，听懂己是不惑年。</span>
-                <el-tag class='tag'>标签一</el-tag>
-                <span class='date'>2019-12-24 09:15:42</span>
-              </div>
-           </el-row>
-       </el-col>
-       <!-- 右侧 -->
-       <el-col :span="6">
-           <el-row class='right' type='flex' justify="end">
-               <span><i class="el-icon-edit"></i>修改</span>
-               <span><i class="el-icon-delete"></i> 删除</span>
-           </el-row>
-       </el-col>
+    <el-row
+      v-for="item in list"
+      :key="item.id.toString()"
+      class="article-item"
+      type="flex"
+      justify="space-between"
+    >
+      <!-- 左侧 -->
+      <el-col :span="14">
+        <el-row type="flex">
+          <img src="item.cover.images.length ? item.cover.images[0] : defaultImg" alt />
+          <div class="info">
+            <span>{{ item.title }}</span>
+            <!-- 过滤器不但可以在插值表达中使用 还可以在v-bind表达式中使用 -->
+            <el-tag :type="item.status | filterType" class="tag">{{ item.status | filterStatus }}</el-tag>
+            <span class="date">{{ item.pubdate }}</span>
+          </div>
+        </el-row>
+      </el-col>
+      <!-- 右侧 -->
+      <el-col :span="6">
+        <el-row class="right" type="flex" justify="end">
+          <span>
+            <i class="el-icon-edit"></i>修改
+          </span>
+          <span>
+            <i class="el-icon-delete"></i> 删除
+          </span>
+        </el-row>
+      </el-col>
     </el-row>
   </el-card>
 </template>
@@ -90,7 +99,44 @@ export default {
         channel_id: null, // 默认是空
         dateRange: []
       },
-      channels: [] // 定义一个channels 接收频道
+      channels: [], // 定义一个channels 接收频道
+      list: [], // 接收文章列表数据
+      defaultImg: require('../../assets/img/default.gif')
+    }
+  },
+  filters: {
+    // 处理显示状态
+    filterStatus (value) {
+      // value 是过滤器前面表达式计算得到的值
+      // 文章状态 0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除
+      switch (value) {
+        case 0:
+          return '草稿'
+        case 1:
+          return '待审核'
+        case 2:
+          return '已发表'
+        case 3:
+          return '审核失败'
+        default:
+          break
+      }
+    },
+    filterType (value) {
+      // value 是过滤器前面表达式计算得到的值
+      // 文章状态 0-草稿，1-待审核，2-审核通过，3-审核失败，4-已删除
+      switch (value) {
+        case 0:
+          return 'warning'
+        case 1:
+          return 'info'
+        case 2:
+          return ''
+        case 3:
+          return 'danger'
+        default:
+          break
+      }
     }
   },
   methods: {
@@ -101,10 +147,18 @@ export default {
       }).then(result => {
         this.channels = result.data.channels // 获取频道数据
       })
+    },
+    // 获取文章列表数据
+    getArticles () {
+      this.$axios({
+        url: '/articles' // 请求地址
+      }).then(result => {
+        this.list = result.data.results // 接收文章列表数据
+      })
+    },
+    created () {
+      this.getArticles() // 调用获取频道数据
     }
-  },
-  created () {
-    this.getChannels() // 调用获取频道数据
   }
 }
 </script>
@@ -115,41 +169,41 @@ export default {
     height: 60px;
     padding-left: 50px;
   }
-    .total {
-      margin:60px 0;
-      height: 30px;
-      border-bottom: 1px dashed #ccc;
+  .total {
+    margin: 60px 0;
+    height: 30px;
+    border-bottom: 1px dashed #ccc;
   }
   .article-item {
-      margin: 20px 0;
-      padding: 10px 0;
-      border-bottom: 1px solid #f2f3f5;
-      img {
-          width: 180px;
-          height: 100px;
-          margin-right: 10px;
-          border-radius: 4px;
+    margin: 20px 0;
+    padding: 10px 0;
+    border-bottom: 1px solid #f2f3f5;
+    img {
+      width: 180px;
+      height: 100px;
+      margin-right: 10px;
+      border-radius: 4px;
+    }
+    .info {
+      height: 100px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-between;
+      .tag {
+        max-width: 80px;
       }
-      .info {
-          height: 100px;
-          display: flex;
-          flex-direction: column;
-          justify-content: space-between;
-          .tag {
-              max-width:80px;
-          }
-          .date {
-              color: #999;
-              font-size:12px;
-          }
+      .date {
+        color: #999;
+        font-size: 12px;
       }
-      .right {
-          span {
-              margin-left:8px;
-              font-size: 14px;
-              cursor: pointer;
-          }
+    }
+    .right {
+      span {
+        margin-left: 8px;
+        font-size: 14px;
+        cursor: pointer;
       }
+    }
   }
 }
 </style>
